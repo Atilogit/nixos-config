fd5ebfe4d3e557b6ce26ef3bf65330da3c3e03d9
Yannick Wagner <52137800+Atilogit@users.noreply.github.com>
2025-07-21 00:06:54 +0200

    Restart wlsunset on resume

--- config/programs/hyprland/utils.nix
+++ config/programs/hyprland/utils.nix
@@ -7,6 +7,17 @@
     longitude = 9.1;
     temperature.night = 3000;
   };
+  systemd.services.wlsunset-resume = {
+    unitConfig = {
+      Description = "Restart wlsunset on resume";
+      After = [ "suspend.target" ];
+    };
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "systemctl --user --machine=atilo@.host restart wlsunset.service";
+    };
+    wantedBy = [ "suspend.target" ];
+  };
 
   # Bar
   home-manager.users.atilo.programs.hyprpanel = {
