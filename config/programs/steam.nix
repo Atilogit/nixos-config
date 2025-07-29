30267005514b85e63976dbe859bb34cd12bf80d9
Yannick Wagner <52137800+Atilogit@users.noreply.github.com>
2025-07-30 01:18:07 +0200

    Steam command to save shader cache

--- config/programs/steam.nix
+++ config/programs/steam.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
-  # PROTON_ENABLE_WAYLAND=1 DXVK_HUD=compiler gamemoderun %command%
+  # __GL_SHADER_DISK_CACHE_SKIP_CLEANUP=1 PROTON_ENABLE_WAYLAND=1 DXVK_HUD=compiler gamemoderun %command%
   programs.gamemode.enable = true;
   programs.steam = {
     enable = true;
