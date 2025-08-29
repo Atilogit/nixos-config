c061efd768a7cfd30107f796aeef681d76ffdbcd
Yannick Wagner <52137800+Atilogit@users.noreply.github.com>
2025-08-29 22:31:28 +0200

    More fonts

--- config/fonts.nix
+++ config/fonts.nix
@@ -3,7 +3,11 @@
   fonts.packages = with pkgs; [
     nerd-fonts.fira-code
     nerd-fonts.caskaydia-cove
+    nerd-fonts.space-mono
+    nerd-fonts.jetbrains-mono
     font-awesome_6
     noto-fonts
+    rubik
+    material-symbols
   ];
 }
