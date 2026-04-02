0f872699de559a04bd50df147df6b3dc47c2f4b8
Yannick Wagner <52137800+Atilogit@users.noreply.github.com>
2026-04-02 18:55:29 +0200

    Jack and Alsa

--- config/services/default.nix
+++ config/services/default.nix
@@ -14,7 +14,6 @@
       value = "unlimited";
     }
   ];
-  services.pipewire.enable = true; # Audio
   services.playerctld.enable = true; # Audio controls
   services.printing.enable = true; # CUPS
 }
