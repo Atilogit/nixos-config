8e706a365add127804fb0edadf4c57ae2673d647
Yannick Wagner <52137800+Atilogit@users.noreply.github.com>
2026-06-24 02:08:00 +0200

    Update osu!

--- modules/common/osu/osu.nix
+++ modules/common/osu/osu.nix
@@ -4,15 +4,26 @@
     # Mostly from https://git.dtth.ch/nki/nix-home/src/branch/master/home/osu/default.nix
     { pkgs, lib, ... }:
     let
+      osu-script-inner = pkgs.writeShellScript "osu-script-inner" ''
+        # test -n "$PIPEWIRE_ALSA" || export PIPEWIRE_ALSA='{ application.process.id='"$$"' alsa.channels=2 alsa.rate=44100 alsa.buffer-bytes=1024 alsa.period-bytes=128 }'
+        # test -n "$PIPEWIRE_LATENCY" || export PIPEWIRE_LATENCY='1/44100'
+        export SDL_VIDEODRIVER=x11
+        export OSU_SDL3=1
+
+        exec "$@"
+      '';
+      osu-script = pkgs.writeShellScript "osu-script" ''
+        exec gamemoderun chrt -r 70 ${osu-script-inner} osu!
+      '';
       osu-pkg =
         with pkgs;
         with lib;
         appimageTools.wrapType2 rec {
           pname = "osu-lazer-bin";
-          version = "2026.429.0-lazer";
+          version = "2026.620.0-lazer";
           src = fetchurl {
             url = "https://github.com/ppy/osu/releases/download/${version}/osu.AppImage";
-            hash = "sha256-3yBDCMffgWQKmBHETYl7IrvT5BOE6vN+sH8dGg+w//s=";
+            hash = "sha256-rLom/UwqVOXUk/ayLvekRQMD49p5MB9BA6RCohtuPfg=";
             # hash = lib.fakeHash; # Use when updating
           };
           extraPkgs = pkgs: with pkgs; [ icu ];
@@ -22,7 +33,8 @@
               contents = appimageTools.extract { inherit pname version src; };
             in
             ''
-              mv -v $out/bin/${pname} $out/bin/osu\!
+              sed "s#osu!#$out/bin/${pname}#g" ${osu-script} > $out/bin/osu!
+              chmod +x $out/bin/osu!
               install -m 444 -D ${contents}/osu\!.desktop -t $out/share/applications
               install -m 444 -D ${./mimetypes.xml} $out/share/mime/packages/${pname}.xml
               for i in 16 32 48 64 96 128 256 512 1024; do
