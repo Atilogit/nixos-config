eca32779a2b7b18fb5c0fb353261fac2135ce0c1
Yannick Wagner <52137800+Atilogit@users.noreply.github.com>
2026-09-22 19:49:45 +0200

    Fix suspend

--- modules/hosts/baller/configuration.nix
+++ modules/hosts/baller/configuration.nix
@@ -29,6 +29,19 @@
       boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore-lto-zen4;
 
       boot.kernelModules = [ "ntsync" ];
+
+      # Suspend
+      # cat /proc/acpi/wakeup
+      # sudo cat /sys/kernel/debug/wakeup_sources
+      services.udev.extraRules = ''
+        ACTION=="add", SUBSYSTEM=="pci", KERNEL=="0000:6d:00.3", ATTR{power/wakeup}="disabled"
+      '';
+      boot.kernelParams = [
+        "nvme_core.default_ps_max_latency_us=0"
+        "nvme_core.mpoll=0"
+        "acpi_enforce_resources=lax"
+      ];
+
       powerManagement = {
         enable = true;
         cpuFreqGovernor = "performance";
