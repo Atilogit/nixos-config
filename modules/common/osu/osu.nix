13455bce057777f327765c65a9144376e3df44bf
Yannick Wagner <52137800+Atilogit@users.noreply.github.com>
2026-09-22 19:35:27 +0200

    osu! update

--- modules/common/osu/osu.nix
+++ modules/common/osu/osu.nix
@@ -20,10 +20,10 @@
         with lib;
         appimageTools.wrapType2 rec {
           pname = "osu-lazer-bin";
-          version = "2026.620.0-lazer";
+          version = "2026.804.2-lazer";
           src = fetchurl {
             url = "https://github.com/ppy/osu/releases/download/${version}/osu.AppImage";
-            hash = "sha256-rLom/UwqVOXUk/ayLvekRQMD49p5MB9BA6RCohtuPfg=";
+            hash = "sha256-0K/dyvIwrlBzcexYDCCilNknJdEZja1OTfAotP6MvjY=";
             # hash = lib.fakeHash; # Use when updating
           };
           extraPkgs = pkgs: with pkgs; [ icu ];
